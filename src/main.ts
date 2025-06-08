fb74c407a3162a62dc4cb82c624a233664bddda1
Ramdhan Setiadhi <ramdhansetiadhi@gmail.com>
2025-06-08 16:59:33 +0700

    remove api prefix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
