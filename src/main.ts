ee05ee5a384c101822f59d9f80761a76fdf78cf9
Ramdhan Setiadhi <ramdhansetiadhi@gmail.com>
2025-06-08 15:54:39 +0700

    cors edit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
